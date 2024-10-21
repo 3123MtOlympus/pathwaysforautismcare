f9c667158a2c42c052c96b553128af0d5a4dbded
3123MtOlympus <realchrislondon@gmail.com>
2024-10-21 10:58:42 -0700

    first draft

--- asset/script.js
+++ asset/script.js
@@ -56,3 +56,37 @@ function signIn() {
         alert('Invalid username or password.');
     }
 }
+$(document).ready(function(){
+    $('.service-cards').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
